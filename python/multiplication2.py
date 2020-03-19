3aacfa29ae115b101eeab00ad68ea7e32e42e9aa
constantine <864917>
2020-03-19 12:08:52 +0800

    multiplication

---
+++ python/multiplication2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("_" * 50)
+i = 1
+while i < 11:
+    n = 1
+    while n <= 10 :
+        print("{:5d}".format(i * n),end=' ')
+        n += 1
+    print()
+    i += 1
+print("_" * 50)
